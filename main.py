5704dd3fed1cb3c5ca4a0b7490400739b3f12454
Bubbles The Dev <152947339+KernFerm@users.noreply.github.com>
2025-10-13 15:13:41 -0400

    Fix formatting and improve sound selection logic
    Signed-off-by: Bubbles The Dev <152947339+KernFerm@users.noreply.github.com>

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def countdown_timer():
     button_font = pygame.font.SysFont(None, 30)
 
     screen = pygame.display.set_mode((600, 700))
-    pygame.display.set_caption('Bubbles The Dev - Countdown Timer')
+    pygame.display.set_caption('BubblesTheDev - Countdown Timer')
 
     pygame.mixer.init()
 
@@ -145,3 +145,4 @@ def countdown_timer():
 
 if __name__ == "__main__":
     countdown_timer()
+
